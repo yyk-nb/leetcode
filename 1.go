af00ea8899f5d48207ba48fea44784fe6b14efb1
yyk-nb <64244067+yyk-nb@users.noreply.github.com>
2022-03-22 21:57:08 +0800

    Create 1.go

---
+++ 1.go
@@ -0,0 +1,15 @@
+func twoSum(nums []int, target int) []int {
+	var result []int
+    for i := 0; i < len(nums); i++{
+		for j := i + 1; j < len(nums); j++{
+			if nums[i] + nums[j] == target{
+				// result[0] = i
+				result = append(result, i)
+				// result[1] = j
+				result = append(result, j)
+				return result
+			}
+		}
+	}
+	return result
+}
